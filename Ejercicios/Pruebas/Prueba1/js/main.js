1ea0f7f1e64939be668d2d064d8d82adcfba4e19
jruisot1903 <jruisot1903@g.educaand.es>
2026-09-18 13:05:34 +0200

    Prueba 1

--- Ejercicios/Pruebas/Prueba1/js/main.js
+++ Ejercicios/Pruebas/Prueba1/js/main.js
@@ -1,2 +1,31 @@
 const mainHTML = document.getElementById("main");
 
+    const btnColor = document.createElement("button");
+
+    btnColor.innerHTML = "Cambiar color Main";
+    btnColor.style.borderRadius = "5px";
+    btnColor.style.border = "1px solid black";
+    btnColor.style.padding = "0.5%";
+    btnColor.style.marginTop = "10px";
+   
+
+    mainHTML.appendChild(btnColor);
+
+    btnColor.addEventListener("click", function (){
+        
+        var randomColor = Math.floor(Math.random()*16777215).toString(16);
+        mainHTML.style.backgroundColor = "#" + randomColor;
+
+    })
+
+    btnColor.addEventListener("mouseover", function(){
+        btnColor.style.opacity = "0.5";
+    })
+
+    btnColor.addEventListener("mouseout", function(){
+        btnColor.style.opacity = "1";
+        
+    })
+
+
+
